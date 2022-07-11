e789f3918daac7a8f93119f51f7d486f85ee4ac5
Rasmus Brandt <rasmusbrandt92@gmail.com>
2022-07-11 19:53:55 +0200

    first commit

---
+++ qFlipper/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64 = 'https://update.flipperzero.one/builds/qFlipper/1.1.0/qFlipperSetup-64bit-1.1.0.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  url64bit       = $url64
+  softwareName   = 'qFlipper*'
+  checksum       = ''
+  checksumType   = 'sha256' 
+  checksum64     = 'A0EF36FDF783C8DDD0322409DA051C6942036C42AF137AF626074ADE3295792F'
+  checksumType64 = 'sha256' 
+  silentArgs     = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs 
+
